95853111778b40fd6eaf2bd8c9ba9c7d8e32e062
Priadkomari48 <128245032+Priadkomari48@users.noreply.github.com>
2024-01-14 10:46:02 +0000

    add

---
+++ src/concatenation.ts
@@ -0,0 +1,15 @@
+function concatenation(firstWord: string, secondWord: string) {
+  console.log(`${firstWord} ${secondWord}`);
+}
+
+export { concatenation };
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false;
+notSure = {};
+
+let num: number;
+
+notSure = [];
+
+export {};
